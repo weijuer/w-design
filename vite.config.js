cd89d0c0883ea20981803397740713ec46ff3027
Weijuer <ch_weijuer@163.com>
2021-08-17 19:49:43 +0800

    🔧 配置依赖升级

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,6 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { resolve as resovePath } from 'path'
-
-const resolve = (dir) => {
-  return resovePath(__dirname, dir)
-}
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
