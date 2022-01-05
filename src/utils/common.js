5e527067292d36e1f83ace1d4996f3d9a3e47b88
Weijuer <ch_weijuer@163.com>
2022-01-05 17:56:29 +0800

    📦 1.优化引入方案

--- src/utils/common.js
+++ src/utils/common.js
@@ -2,8 +2,8 @@
  * 是否是中文
  * @returns
  */
-const charRegx = /^[\u4e00-\u9fa5]$/
-export const isCNChar = charRegx.test.bind(charRegx)
+const charRegx = /^[\u4e00-\u9fa5]$/;
+export const isCNChar = charRegx.test.bind(charRegx);
 
 /**
  * 获取modules
@@ -11,11 +11,47 @@ export const isCNChar = charRegx.test.bind(charRegx)
  * @returns
  */
 export const getAllModules = (path) => {
-  const modules = import.meta.globEager(path + '')
-  const res = {}
+  const modules = import.meta.globEager(path + '');
+  const res = {};
   for (const { path, mod } of modules) {
-    const name = path.split('/')[1]
-    res[`w-${name}`] = mod?.default
+    const name = path.split('/')[1];
+    res[`w-${name}`] = mod?.default;
   }
-  return { res }
+  return { res };
+};
+
+/**
+ * 引入当前文件夹及其子目录下所有的.vue文件
+ * @param {Object} modules
+ * @returns
+ */
+export function generateModules(modules) {
+  const _modules = {};
+  Object.entries(modules).map(([fileName, { default: component }]) => {
+    const [, name] = fileName.match(/(?:\/([\w-]+)\/)?(?:[\w-]+)\.js$/);
+    _modules[`W${firstToUpper(name)}`] = component;
+  });
+  return _modules;
+}
+
+/**
+ * 根据文件夹生成路由
+ */
+export function generateRoutes() {
+  // 引入views下及其子目录下所有的.vue文件
+  const modules = import.meta.glob('../views/**/*.vue');
+  return Object.entries(modules).map(([fileName, component]) => {
+    const [, parent, name] = fileName.match(/(?:views\/([\w-]+)\/)?([\w-]+)\.vue$/);
+    const path = name === 'NotFound' ? '/:pathMatch(.*)*' : `/${name}`;
+    return parent ? { name, path, parent, component } : { name, path, children: [], component };
+  });
+}
+
+/**
+ * 字符串首字母大写
+ * @param {String} str
+ * @returns
+ */
+function firstToUpper(str) {
+  return str.toLowerCase().replace(/( |^)[a-z]/g, (L) => L.toUpperCase());
 }
