ea0a028116de070e2c7b46c0fd7b98636bb9932d
Weijuer <ch_weijuer@163.com>
2022-01-12 10:32:33 +0800

    🎨 1.修改配置文件

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import legacy from '@vitejs/plugin-legacy';
 import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [
-    vue(),
-    legacy({
-      targets: ['defaults', 'IE 10']
-    })
+    vue()
   ],
   resolve: {
     alias: {
