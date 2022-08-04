09cc5d426a6bf007f5934b73b28a48a2dce2dda0
Weijuer <ch_weijuer@163.com>
2022-08-04 10:00:50 +0800

    🎨 1.update deps and workflows

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
 import router from './router';
 import wd from 'packages';
 import { worker } from './mocks/browser';
 
 // Start the mocking conditionally.
 if (process.env.NODE_ENV === 'development') {
-  // worker.start()
+  // worker.start();
 }
 
 const app = createApp(App);
