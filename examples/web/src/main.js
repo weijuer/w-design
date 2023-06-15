91acb34feef03a130cc854015d6caad51b591f22
Weijuer <ch_weijuer@163.com>
2023-06-15 11:17:25 +0800

    feat: 1.update devDependencies; 2.remove unnecessary deps.

--- examples/web/src/main.js
+++ examples/web/src/main.js
@@ -1,11 +1,13 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import wd from '@w-design/w-design-vue';
-import 'virtual:svg-icons-register';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-const app = createApp(App);
+import 'virtual:svg-icons-register'
+import '@w-design/w-design-vue/index.css'
+import wd from '@w-design/w-design-vue'
 
-app.use(router);
-app.use(wd);
-app.mount('#app');
+const app = createApp(App)
+
+app.use(router)
+app.use(wd)
+app.mount('#app')
