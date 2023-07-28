977dd174257f0d3ea75ffafbaf33efc3e2897db2
Weijuer <ch_weijuer@163.com>
2023-07-28 14:22:31 +0800

    hotfix: 1.update breadcrumb

---
+++ packages/w-design-vue/src/components/breadcrumb/src/breadcrumb.vue
@@ -0,0 +1,27 @@
+<template>
+    <ul ref="_ref" class="w-breadcrumb">
+        <slot></slot>
+    </ul>
+</template>
+  
+<script lang="ts">
+export default {
+    name: 'w-breadcrumb'
+}
+</script>
+  
+<script lang="ts" setup>
+import { provide, ref } from 'vue';
+import { breadcrumbProps } from './interface';
+
+const props = defineProps(breadcrumbProps);
+const _ref = ref<HTMLUListElement>()
+
+provide('breadcrumb', props);
+
+defineExpose({
+    ref: _ref
+})
+</script>
+  
+<style src="./breadcrumb.scss" lang="scss" />
